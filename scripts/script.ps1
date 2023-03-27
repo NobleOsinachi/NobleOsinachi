618a705ee21e114b66bce5be2945dadb5024e15e
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-27 16:57:15 +0100

    Created bug-fix branch
    Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    
    Co-authored-by: David Reiss <dreiss@fb.com>

--- scripts/script.ps1
+++ scripts/script.ps1
@@ -5,11 +5,11 @@ cd "C:/Users/Noble/SOURCES/REPOS/test"
 
 git pull origin main
 
-git checkout -b ps-patch-12
+git checkout -b bug-fix
 
 git add .
 
-git commit -m "Created ps-patch-12 branch 
+git commit -m "Created bug-fix branch 
 
 Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
 Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
@@ -20,12 +20,12 @@ Co-authored-by: David Reiss <dreiss@fb.com>
 
 git pull origin main
 
-git push -u origin ps-patch-12
+git push -u origin bug-fix
 
-hub pull-request -m "Created ps-patch-12 branch" -h patch-2 -b main -f
+hub pull-request -m "Created bug-fix branch" -h bug-fix -b main -f
 
 # Only delete branch after all pulls have been merged by Golden
-# git branch -d ps-patch-12
+# git branch -d bug-fix
 
-# git push origin --delete ps-patch-12
+# git push origin --delete bug-fix
 
