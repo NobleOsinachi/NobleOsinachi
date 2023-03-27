35e74bf41e35b8beb87c29058de79711918a5d2d
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-27 16:28:10 +0100

    Created 5685f4ce branch

---
+++ scripts/5685f4ce-a505-4e03-8b60-97455e26099c.cmd
@@ -0,0 +1,31 @@
+
+color 0a
+
+cd "C:/Users/Noble/SOURCES/REPOS/test"
+
+git pull origin main
+
+git checkout -b 5685f4ce
+
+git add .
+
+git commit -m "Created 5685f4ce branch 
+
+Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: iAmGolden <chuksgold29@gmail.com>
+
+Co-authored-by: David Reiss <dreiss@fb.com>
+"
+
+git pull origin main
+
+git push -u origin 5685f4ce
+
+hub pull-request -m "Created 5685f4ce branch" -h patch-2 -b main -f
+
+# Only delete branch after all pulls have been merged by Golden
+# git branch -d 5685f4ce
+
+# git push origin --delete 5685f4ce
+
