1eebe171d2e8d88e4f05fcd07fca072358c6b228
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-28 16:00:13 +0100

    Created ebb22243 branch
    Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>

--- scripts/ebb22243-c8b0-40ec-843d-70336ec36f8c.ps1
+++ scripts/ebb22243-c8b0-40ec-843d-70336ec36f8c.ps1
@@ -3,11 +3,11 @@ cd "C:\Users\Noble\SOURCES\REPOS\test"
 
 git pull origin main
 
-git checkout -b 97909320
+git checkout -b ebb22243
 
 git add .
 
-git commit -m "Created 97909320 branch
+git commit -m "Created ebb22243 branch
 
 Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
 Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
@@ -18,11 +18,11 @@ Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
 
 git pull origin main
 
-git push -u origin 97909320
+git push -u origin ebb22243
 
-hub pull-request -m "Created 97909320 branch" -h 97909320 -b main -f
+hub pull-request -m "Created ebb22243 branch" -h ebb22243 -b main -f
 
 # Only delete branch after all pulls have been merged by Golden
-# git branch -d 97909320
+# git branch -d ebb22243
 
-# git push origin --delete 97909320
+# git push origin --delete ebb22243
