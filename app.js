3c35da68d2e562ad83fa1647cbeabc26fba630bd
Noble Osinachi <150407018@live.unilag.edu.ng>
2023-03-28 13:38:03 +0100

    Created ${branchName} branch (#16)

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 
 const fileName = uuidv4();
 const branchName = fileName.substring(0, 8);
-const fileContent = `
-# color 0a
+const fileContent = `color 0a
 
-cd "C:/Users/Noble/SOURCES/REPOS/test"
+cd "C:\\Users\\Noble\\SOURCES\\REPOS\\test"
 
 git pull origin main
 
@@ -15,15 +14,17 @@ git checkout -b ${branchName}
 
 git add .
 
-git commit -m "Created ${branchName} branch \n\nCo-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>\nCo-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>\nCo-authored-by: iAmGolden <chuksgold29@gmail.com>
-\nCo-authored-by: David Reiss <dreiss@fb.com>
+git commit -m "Created ${branchName} branch \n\nCo-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>\nCo-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>\nCo-authored-by: iAmGolden <chuksgold29@gmail.com>\nCo-authored-by: Ibukun <ibukunoladipupo10@gmail.com>\nCo-authored-by: Sanowar Sunny <ibukunoladipupo10@gmail.com>
+
+SANOWAR HOSSAIN SUNNY sanowarhossain3843@gmail.com
+
 "
 
 git pull origin main
 
 git push -u origin ${branchName}
 
-hub pull-request -m "Created ${branchName} branch" -h patch-2 -b main -f
+hub pull-request -m "Created ${branchName} branch" -h ${branchName} -b main -f
 
 # Only delete branch after all pulls have been merged by Golden
 # git branch -d ${branchName}
@@ -40,7 +41,7 @@ fs.writeFile(file, fileContent, (err) => {
     console.log('The file has been created!');
 
     // execute the cmd file in a new command prompt window
-    let command = `start powershell /c ${file}`;
+    let command = `start cmd /c ${file}`;
     console.log(command);
 
     exec(command, (error, stdout, stderr) => {
