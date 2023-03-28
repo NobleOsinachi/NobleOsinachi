e02465b8ec329edbcab1142be6fe3893cf030ff1
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-28 11:09:46 +0100

    Created ${branchName} branch \n\nCo-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>\nCo-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>\nCo-authored-by: iAmGolden <chuksgold29@gmail.com>\nCo-authored-by: Ibukun <ibukunoladipupo10@gmail.com>\nCo-authored-by: Sanowar Sunny <ibukunoladipupo10@gmail.com>

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const fs = require('fs');
 
 const fileName = uuidv4();
 const branchName = fileName.substring(0, 8);
-const fileContent = `
-color 0a
-
-powershell
+const fileContent = `color 0a
 
 cd "C:\\Users\\Noble\\SOURCES\\REPOS\\test"
 
@@ -17,7 +14,11 @@ git checkout -b ${branchName}
 
 git add .
 
-git commit -m "Created ${branchName} branch \n\nCo-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>\nCo-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>\nCo-authored-by: iAmGolden <chuksgold29@gmail.com>\nCo-authored-by: Ibukun <ibukunoladipupo10@gmail.com>"
+git commit -m "Created ${branchName} branch \n\nCo-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>\nCo-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>\nCo-authored-by: iAmGolden <chuksgold29@gmail.com>\nCo-authored-by: Ibukun <ibukunoladipupo10@gmail.com>\nCo-authored-by: Sanowar Sunny <ibukunoladipupo10@gmail.com>
+
+SANOWAR HOSSAIN SUNNY sanowarhossain3843@gmail.com
+
+"
 
 git pull origin main
 
@@ -40,7 +41,7 @@ fs.writeFile(file, fileContent, (err) => {
     console.log('The file has been created!');
 
     // execute the cmd file in a new command prompt window
-    let command = `start powershell /c ${file}`;
+    let command = `start cmd /c ${file}`;
     console.log(command);
 
     exec(command, (error, stdout, stderr) => {
