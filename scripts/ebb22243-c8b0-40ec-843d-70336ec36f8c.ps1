a00f65e22e00fba5d4ee0dc629d5468102990463
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-29 04:36:04 +0100

    Created 81004f91 branch (#36)

---
+++ scripts/ebb22243-c8b0-40ec-843d-70336ec36f8c.ps1
@@ -0,0 +1,28 @@
+
+cd "C:\Users\Noble\SOURCES\REPOS\test"
+
+git pull origin main
+
+git checkout -b ebb22243
+
+git add .
+
+git commit -m "Created ebb22243 branch
+
+Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: iAmGolden <chuksgold29@gmail.com>
+Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
+Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
+"
+
+git pull origin main
+
+git push -u origin ebb22243
+
+hub pull-request -m "Created ebb22243 branch" -h ebb22243 -b main -f
+
+# Only delete branch after all pulls have been merged by Golden
+# git branch -d ebb22243
+
+# git push origin --delete ebb22243
