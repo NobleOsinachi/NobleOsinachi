e1964fcb3a5c71edcad6da0df1d6ba92a0d9a7c4
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-29 02:13:27 +0100

    Created 8c9b60b7 branch
    Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>

---
+++ scripts/8c9b60b7-de73-4885-a084-6764ce4dc36a.ps1
@@ -0,0 +1,28 @@
+
+cd "C:\Users\Noble\SOURCES\REPOS\test"
+
+git pull origin main
+
+git checkout -b 8c9b60b7
+
+git add .
+
+git commit -m "Created 8c9b60b7 branch
+
+Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: iAmGolden <chuksgold29@gmail.com>
+Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
+Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
+"
+
+git pull origin main
+
+git push -u origin 8c9b60b7
+
+hub pull-request -m "Created 8c9b60b7 branch" -h 8c9b60b7 -b main -f
+
+# Only delete branch after all pulls have been merged by Golden
+# git branch -d 8c9b60b7
+
+# git push origin --delete 8c9b60b7
