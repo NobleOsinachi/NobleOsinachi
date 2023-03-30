8c025aff3b8e22be3196fd863c9cd46bfd75e18c
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-30 20:00:13 +0100

    Created 7666a4fa branch
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
    Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: PraiseGeek <praisegeek@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain343@gmail.com>
    Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
    Co-authored-by: Youngsup Kim <idebtor@gmail.com>
    Co-authored-by: @UchenduOluoma <oluoma_u@gmail.com>
    Co-authored-by: Uchendu Oluoma <oluomachizaram@gmail.com>

---
+++ scripts/7666a4fa-407f-465e-9f9b-427a3e0f0bd9.ps1
@@ -0,0 +1,34 @@
+
+cd "C:\Users\Noble\SOURCES\REPOS\test"
+
+git pull origin main
+
+git checkout -b 7666a4fa
+
+git add .
+
+git commit -m "Created 7666a4fa branch
+
+Co-authored-by: iAmGolden <chuksgold29@gmail.com>
+Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
+Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
+Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: PraiseGeek <praisegeek@gmail.com>
+Co-authored-by: Sanowar Sunny <sanowarhossain343@gmail.com>
+Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
+Co-authored-by: Youngsup Kim <idebtor@gmail.com>
+Co-authored-by: @UchenduOluoma <oluoma_u@gmail.com>
+Co-authored-by: Uchendu Oluoma <oluomachizaram@gmail.com>
+"
+
+git pull origin main
+
+git push -u origin 7666a4fa
+
+hub pull-request -m "Created 7666a4fa branch" -h 7666a4fa -b main -f
+
+# Only delete branch after all pulls have been merged by Golden
+# git branch -d 7666a4fa
+
+# git push origin --delete 7666a4fa
