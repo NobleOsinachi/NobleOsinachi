857298f1271a1a9463fb75f8e02f23ab4e62c653
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-02 13:07:25 -0700

    Created df580aa1 branch (#72)

---
+++ scripts/286189a4-1f8b-4922-bc3e-71316a3f2f52.ps1
@@ -0,0 +1,37 @@
+
+cd "C:\Users\Noble\SOURCES\REPOS\test"
+
+git pull origin main
+
+git checkout -b 286189a4
+
+git add .
+
+git commit -m "Created 286189a4 branch
+
+Co-authored-by: Noble Osinachi <nobleosinachi98@gmail.com>
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: sifatfaysal <sifatfaysall@gmail.com>
+Co-authored-by: Alireza Zahiri <itsalireza.zr@gmail.com>
+Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
+Co-authored-by: Golden Chukwukere <66950981+iamGolden@users.noreply.github.com>
+Co-authored-by: David Popoola <davieedpopoola@gmail.com>
+Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
+Co-authored-by: Uchenna Ogbu <boluogbu@gmail.com>
+Co-authored-by: NobleOsinachi <150407018@live.unilag.edu.ng>
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: iAmGolden <chuksgold29@gmail.com>
+Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
+Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
+"
+
+git pull origin main
+
+git push -u origin 286189a4
+
+hub pull-request -m "Created 286189a4 branch" -h 286189a4 -b main -f
+
+# Only delete branch after all pulls have been merged by Golden
+# git branch -d 286189a4
+
+# git push origin --delete 286189a4
