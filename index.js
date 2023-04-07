9ef23c1945ba8ac5ef2c17c61afd6e32e5bac2d5
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-07 12:55:23 +0100

    Updated collaborators list

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+class Program {
+    static main() {
+        // let emp = new Employee("Noble", "1998-03-02");
+        console.log("Hello World");
+    }
+}
+(() => {
+    Program.main();
+})();
